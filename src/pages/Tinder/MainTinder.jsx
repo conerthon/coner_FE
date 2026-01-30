b89b4d78a9e9a8eabb7e9487c30d6779ad304b6e
yeoni <happyksy0501@gmail.com>
2026-01-31 01:19:32 +0900

    폴더 및 파일 이름 오류 정정

---
+++ src/pages/Tinder/MainTinder.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const PickGroup = () => {
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+export default PickGroup
--- src/pages/Tinder/ThereisNoPicture.jsx
+++ src/pages/Tinder/ThereisNoPicture.jsx
@@ -1,1 +1,11 @@
 //사진이 없으면 보여주는 부분
+import React from 'react';
+const PickGroup = () => {
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+export default PickGroup
